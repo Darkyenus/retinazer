ffd223754c384dbb58d5eee9e4efe53cd85e97ef
Darkyenus <darkyenus@gmail.com>
2016-07-23 15:01:15 +0200

    Add EntityListenerAdapter interface

--- src/main/java/com/github/antag99/retinazer/EntityListener.java
+++ src/main/java/com/github/antag99/retinazer/EntityListener.java
@@ -21,6 +21,8 @@
  ******************************************************************************/
 package com.github.antag99.retinazer;
 
+import com.badlogic.gdx.utils.IntArray;
+
 /**
  * Listener for a {@link Family} or {@link Engine}.
  */
@@ -41,4 +43,42 @@ public interface EntityListener {
      *            the entities that were removed.
      */
     void removed(EntitySetView entities);
+
+    /** Interface adapter implementing most common EntityListener operation - iterating over inserted/removed entities. */
+    interface EntityListenerAdapter extends EntityListener {
+
+        /**
+         * Called for each inserted entity.
+         *
+         * @param entity inserted
+         */
+        void inserted(int entity);
+
+        /**
+         * Called for each removed entity.
+         *
+         * @param entity removed
+         */
+        void removed(int entity);
+
+        @Override
+        default void inserted(EntitySetView entities) {
+            final IntArray indices = entities.getIndices();
+            final int size = indices.size;
+            final int[] items = indices.items;
+            for (int i = 0; i < size; i++) {
+                inserted(items[i]);
+            }
+        }
+
+        @Override
+        default void removed(EntitySetView entities) {
+            final IntArray indices = entities.getIndices();
+            final int size = indices.size;
+            final int[] items = indices.items;
+            for (int i = 0; i < size; i++) {
+                removed(items[i]);
+            }
+        }
+    }
 }
